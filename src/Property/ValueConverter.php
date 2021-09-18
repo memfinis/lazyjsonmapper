7a99a3c2c9125a879c7ba8b2fe1fcbe03404aecb
memfinis <80632669+memfinis@users.noreply.github.com>
2021-09-18 20:48:49 +0300

    Removed convert to simple array

--- src/Property/ValueConverter.php
+++ src/Property/ValueConverter.php
@@ -150,8 +150,7 @@ class ValueConverter
             // Subtract 1 from the remaining array-depth and process current layer.
             $newRemArrayDepth = $remArrayDepth - 1;
             $nextValidKey = 0; // Must start at 0.
-            $simpleArray = array_values($value); // Convert to simple array
-            foreach ($simpleArray as $k => &$v) { // IMPORTANT: By reference!
+            foreach ($value as $k => &$v) { // IMPORTANT: By reference!
                 // OPTIMIZATION: We MUST only allow sequential int keys, but we
                 // avoid the is_int() call by using an int counter instead:
                 /*
